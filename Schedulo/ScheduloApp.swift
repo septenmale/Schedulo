3d0ca4b21759ac242d8a157e7fb5a44c93fc1ff5
septenmale <zavhorodnii.viktor@icloud.com>
2025-05-29 08:08:22 +0200

    first commit

---
+++ Schedulo/ScheduloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduloApp.swift
+//  Schedulo
+//
+//  Created by Viktor Zavhorodnii on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScheduloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
